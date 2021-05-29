9e5038a55e5c451ed28ac4da15b0b2621896d56f
Arjun <gt01arjun@thecea.ca>
2021-05-28 17:23:18 -0700

    Added Truck Controller

---
+++ Assets/Scripts/ParticleDisabler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ParticleDisabler : MonoBehaviour
+{
+    public float delay;
+    private ParticleSystem fx;
+
+    private void Start()
+    {
+        fx = GetComponent<ParticleSystem>();
+    }
+
+    public IEnumerator disable()
+    {
+        yield return new WaitForSeconds(delay);
+        ParticleSystem.EmissionModule em = fx.emission;
+        em.enabled = false;
+    }
+}
