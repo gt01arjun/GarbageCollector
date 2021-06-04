d25b9ac6b8a6c1e24adc01eb82d0cd613a174272
Arjun <gt01arjun@thecea.ca>
2021-06-04 02:07:53 -0700

    Added Pause, Win, Lose Functionality

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using System;
 
 public class GameManager : MonoBehaviour
 {
@@ -14,11 +16,17 @@ public class GameManager : MonoBehaviour
     public TMP_Text TruckCapacityText;
     public TMP_Text ScoreText;
 
+    public GameObject PauseMenuScreen;
+    public GameObject WinScreen;
+    public GameObject LoseScreen;
+
     private void Start()
     {
+        Time.timeScale = 1f;
         CurrentTruckStorage = 0;
         MaxTruckStorage = 10;
         Score = 0;
+        GameOver = false;
     }
 
     void Update()
@@ -32,9 +40,22 @@ public class GameManager : MonoBehaviour
                 TimeRemaining -= Time.deltaTime;
                 DisplayTime(TimeRemaining);
             }
-            else
+            else if (TimeRemaining < 0 && GarbageSpawner.NumberOfGarbages > 0)
             {
                 GameOver = true;
+                GameLose();
+            }
+
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Time.timeScale = 0;
+                PauseMenuScreen.SetActive(true);
+            }
+
+            if (GarbageSpawner.NumberOfGarbages <= 0 && TimeRemaining >= 0)
+            {
+                GameOver = true;
+                GameWin();
             }
         }
     }
@@ -44,5 +65,31 @@ public class GameManager : MonoBehaviour
         float minutes = Mathf.FloorToInt(timeToDisplay / 60);
         float seconds = Mathf.FloorToInt(timeToDisplay % 60);
         TimerText.text = "Time Remaining : " + string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    public void OnResume()
+    {
+        Time.timeScale = 1f;
+        PauseMenuScreen.SetActive(false);
+    }
+
+    public void QuitToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    private void GameLose()
+    {
+        LoseScreen.SetActive(true);
+    }
+
+    private void GameWin()
+    {
+        WinScreen.SetActive(true);
     }
 }
